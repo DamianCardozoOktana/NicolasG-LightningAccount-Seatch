331c75515d6d8b7ae413c14c998de140e7d8d715
Nicolas Gonzalez <nicolasglz@oktana.io.com>
2017-11-10 15:57:38 -0300

    Added pagination to apiCall with items per page

--- src/aura/dummyCall/dummyCallController.js
+++ src/aura/dummyCall/dummyCallController.js
@@ -1,5 +1,37 @@
 ({
-	handleClick : function(component, event, helper) {
-		console.log('Make Dummy call!');
+	makeRequest : function(component, event, helper) {
+		helper.makeHttpRequest(component);
+	},
+	prevPage: function(component, event, helper) {
+		var prev = component.get('v.page');
+
+		if (prev != 1) {
+			prev--;
+		}
+
+		component.set('v.page', prev);
+		helper.makeHttpRequest(component);
+	},
+	nextPage: function(component, event, helper) {
+		var next = component.get('v.page'),
+			lastPage = component.get('c.getLastPage');
+
+		if (next != lastPage) {
+			next++;
+		}
+
+		component.set('v.page', next);
+		helper.makeHttpRequest(component);
+	},
+	getLastPage: function() {
+		var perPage = component.get('v.perPage'),
+			allItems = component.get('v.allItems'),
+			lastPage = allItems/perPage;
+
+		if (lastPage % 1 != 0) {
+			lastPage++;
+		}
+
+		return lastPage;
 	}
 })
