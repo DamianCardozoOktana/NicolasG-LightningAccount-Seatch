709f55fe388ee447eeed639f70a54cec05506b00
Nicolas Gonzalez <nicolasglz@oktana.io.com>
2017-11-13 10:38:17 -0300

    Corrections

--- src/aura/allContacts/allContactsHelper.js
+++ src/aura/allContacts/allContactsHelper.js
@@ -1,4 +1,16 @@
 ({
-	helperMethod : function() {
+	loadContacts : function(component, accountId) {
+		var action = component.get("c.findContactsByAccount");
+
+		action.setParams({"accountID" : accountId});
+		action.setCallback(this, function(response) {
+			var state = response.getState();			
+			if (state === "SUCCESS") {
+				component.set("v.contacts", response.getReturnValue());
+			} else {
+				// ERROR HANDLE
+			}
+		});
+		$A.enqueueAction(action);
 	}
 })
